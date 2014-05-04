c0bc8dee58ace77c2d2bab3fbdffc84357cf916d
bartek <bartlomiej.drozd@uj.edu.pl>
2014-05-05 00:42:36 +0200

    Created class Player

---
+++ src/model/Player.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by bartek on 05.05.14.
+ */
+public class Player{
+    private String name;
+    private Integer money;
+
+    public void setName(String arg1){
+        this.name=arg1;
+    }
+
+    public void setMoney(Integer arg1){
+        this.money=arg1;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Integer getMoney(){
+        return this.money;
+    }
+}
