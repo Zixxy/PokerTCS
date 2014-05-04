7bd9c28f73efd300f9aea0db7be2d1174d228941
bartek <bartlomiej.drozd@uj.edu.pl>
2014-05-05 01:05:32 +0200

    Wyrzucenie Card

--- src/View/ViewInterface.java
+++ src/View/ViewInterface.java
@@ -4,9 +4,12 @@ public interface ViewInterface {
 	public void addPlayer(String name, int id); // możemy się umówić czy playera identyfikujemy imieniem czy id
 	public void removePlayer(int id);
 	public void updatePlayerCash(int id, int cash);
-	public void addThreeCardsOnTable(Card firstCard, Card secondCard, Card thirdCard);
-	public void addOneCard(Card card);
+	public void addThreeCardsOnTable(Integer firstCard, Integer secondCard, Integer thirdCard);
+	public void addOneCard(Integer card);
 	public void clearTable();
-	public void updatePlayerHand(Card firstCardId, Card secondCardId); // w razie nulli player nie ma zadnych kart
+	public void updatePlayerHand(Integer firstCardId, Integer secondCardId); // w razie nulli player nie ma zadnych kart
 	public void updatePlayerLinedCash(int id, int cash);// ustawiamy gotówkę jaką player ma przed sobą na stole
 }
+/* Umawialismy sie, ze nie bedzie takiego obiektu jak Card. Zmieniam na Integera - do pozniejszych zmian.
+Chcę doprowadzić projekt do stanu kompilującego się.
+ */
