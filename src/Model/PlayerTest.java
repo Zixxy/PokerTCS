78fe9781e4a35ecada66cd8de1fb9730ee5ab657
bartek <bartlomiej.drozd@uj.edu.pl>
2014-05-05 01:08:04 +0200

    Created TestOfConstructor

---
+++ src/Model/PlayerTest.java
@@ -0,0 +1,26 @@
+package Model;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by bartek on 05.05.14.
+ */
+
+public class PlayerTest {
+    @Test
+    public void testOfConstructor() throws Exception {
+        Player player=new Player("Krzysztof", 950);
+        assertEquals("Krzysztof", player.getName());
+        assertEquals((Object) 950, player.getMoney());
+        player.setMoney(800);
+        assertEquals("Krzysztof", player.getName());
+        assertEquals((Object) 800, player.getMoney());
+        player.setName("Zdzisław");
+        assertEquals("Zdzisław", player.getName());
+        assertEquals((Object) 800, player.getMoney());
+
+    }
+}
