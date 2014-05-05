464ec320629331c7968a56cad09d0eefe79ece37
Szymon Stankiewicz <dakurels@gmail.com>
2014-05-06 00:23:21 +0200

    Remove throwing exceptions

--- src/main/java/Adapter/AdapterInterface.java
+++ src/main/java/Adapter/AdapterInterface.java
@@ -8,15 +8,15 @@ import main.java.View.ViewInterface;
  */
 
 public interface AdapterInterface {
-    public void fold(int playerId) throws Exception;
-    public void check(int playerId) throws Exception;
-    public void raise(int playerId, int amount) throws Exception;
-    public void resign(int playerId) throws Exception;
-    public void sendMessage(String text) throws Exception;
-    public void addPlayer(String name, int id) throws Exception;
-    public void removePlayer(int id) throws Exception;
-    public void updatePlayerCash(int id, int cash) throws Exception;
-    public void updatePlayerLinedCash(int id, int cash) throws Exception;
-    public void addModel(ModelInterface mod) throws Exception;
-    public void addView(ViewInterface view) throws Exception;
+    public void fold(int playerId);
+    public void check(int playerId);
+    public void raise(int playerId, int amount);
+    public void resign(int playerId);
+    public void sendMessage(String text);
+    public void addPlayer(String name, int id);
+    public void removePlayer(int id);
+    public void updatePlayerCash(int id, int cash);
+    public void updatePlayerLinedCash(int id, int cash);
+    public void addModel(ModelInterface mod);
+    public void addView(ViewInterface view);
 }
