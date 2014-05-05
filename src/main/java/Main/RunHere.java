6856dc6f3f80ee97e3b106c4e5f3d12a895a39b0
bartek <bartlomiej.drozd@uj.edu.pl>
2014-05-06 01:22:53 +0200

    Made Main.RunHere

--- src/main/java/Main/RunHere.java
+++ src/main/java/Main/RunHere.java
@@ -1,10 +1,25 @@
 package main.java.Main;
+import main.java.Adapter.MainAdapter;
+import main.java.Model.ModelOne;
+import main.java.View.TableView;
+import main.java.View.ViewInterface;
+
 
 /**
  * Created by bartek on 05.05.14.
  */
 public class RunHere {
     public static void main(String[] args){
-
+        MainAdapter adapter=new MainAdapter();
+        ViewInterface view = new TableView(adapter,0);
+        ModelOne model = new ModelOne(adapter);
+        adapter.addModel(model);
+        adapter.addView(view);
+        model.addPlayer("Sylwek");
+        model.addPlayer("Maciek");
+        model.addPlayer("Bartek");
+        model.addPlayer("Szymon");
+        model.start();
+        while(true);
     }
 }
---
+++ src/test/ModelOneTest.java
@@ -0,0 +1,20 @@
+package test;
+
+
+import main.java.Model.ModelOne;
+import main.java.Adapter.MainAdapter;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by bartek on 06.05.14.
+ */
+public class ModelOneTest {
+    @Test
+    public void testOfConstructor() throws Exception {
+        MainAdapter adapter = new MainAdapter();
+
+
+    }
+}
