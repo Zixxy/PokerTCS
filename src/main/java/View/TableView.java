b5a6d885708560977e76a1508ae3865d4e769016
Sylwester Klocek <sylwekklocek@gmail.com>
2014-05-05 23:48:29 +0200

    added TableView

---
+++ src/main/java/View/TableView.java
@@ -0,0 +1,31 @@
+package main.java.View;
+
+public class TableView implements ViewInterface{
+	
+	@Override
+	public void addPlayer(String name, int id) {
+	}
+	@Override
+	public void removePlayer(int id) {
+	}
+	@Override
+	public void updatePlayerCash(int id, int cash) {
+	}
+	@Override
+	public void addThreeCardsOnTable(Integer firstCard, Integer secondCard,
+			Integer thirdCard) {
+	}
+	@Override
+	public void addOneCard(Integer card) {
+	}
+	@Override
+	public void clearTable() {
+	}
+	@Override
+	public void updatePlayerHand(Integer firstCardId, Integer secondCardId) {
+	}
+	@Override
+	public void updatePlayerLinedCash(int id, int cash) {
+	}
+	
+}
