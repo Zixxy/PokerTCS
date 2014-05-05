89937045875a2620cf53626a2c55eb647118fbb6
Sylwester Klocek <sylwekklocek@gmail.com>
2014-05-06 00:51:18 +0200

    added three buttons and text field to TableView.

--- src/main/java/View/TableView.java
+++ src/main/java/View/TableView.java
@@ -1,5 +1,14 @@
 package main.java.View;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 public class TableView implements ViewInterface{
 	
 	@Override
@@ -27,10 +36,37 @@ public class TableView implements ViewInterface{
 	@Override
 	public void updatePlayerLinedCash(int id, int cash) {
 	}
-
+	public void start(Stage primaryStage) throws Exception {
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.BOTTOM_RIGHT);
+		grid.setHgap(20);
+		grid.setVgap(0);
+		grid.setPadding(new Insets(10, 20, 10, 200));
+		Button btnpas = new Button("pas");
+		HBox pas = new HBox(50);
+		pas.setAlignment(Pos.BOTTOM_RIGHT);
+		pas.getChildren().add(btnpas);
+		grid.add(pas, 2, 1);
+		Button btnRaise = new Button("Raise");
+		HBox Raise = new HBox(50);
+		Raise.setAlignment(Pos.BOTTOM_RIGHT);
+		Raise.getChildren().add(btnRaise);
+		grid.add(Raise, 1, 1);
+		Button btnCheck = new Button("Check");
+		HBox Check = new HBox(50);
+		Check.setAlignment(Pos.BOTTOM_RIGHT);
+		Check.getChildren().add(btnCheck);
+		grid.add(Check, 3, 1);
+		TextField userCashTextField = new TextField();
+	    grid.add(userCashTextField, 0, 1);
+		Scene scene = new Scene(grid, 800, 600);
+	//	grid.setGridLinesVisible(true);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
     @Override
     public void sendMessage(String text) throws Exception {
-
     }
 
 }
