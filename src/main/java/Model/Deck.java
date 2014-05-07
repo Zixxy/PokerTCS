08df1292e74a116646fe0fbae16678010344f4df
Szymon Stankiewicz <dakurels@gmail.com>
2014-05-07 16:20:45 +0200

    Added id to Card.Value

--- src/main/java/Model/Deck.java
+++ src/main/java/Model/Deck.java
@@ -9,19 +9,23 @@ import java.util.List;
 public class Deck {
     public static class Card implements Comparable<Card>{
         public enum Value {
-            TWO,
-            THREE,
-            FOUR,
-            FIVE,
-            SIX,
-            SEVEN,
-            EIGHT,
-            NINE,
-            TEN,
-            JACK,
-            QUEEN,
-            KING,
-            ACE
+            TWO(2),
+            THREE(3),
+            FOUR(4),
+            FIVE(5),
+            SIX(6),
+            SEVEN(7),
+            EIGHT(8),
+            NINE(9),
+            TEN(10),
+            JACK(11),
+            QUEEN(12),
+            KING(13),
+            ACE(14);
+            Value(int id) {
+                this.id = id;
+            }
+            public final int id;
         }
         public enum Color {
             CLUBS,
