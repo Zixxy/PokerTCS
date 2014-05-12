e6754fdb228094fec5a9ff3e3ea2b03aa9fb4c73
arytmetyk <bartlomiej.drozd@uj.edu.pl>
2014-05-12 21:05:32 +0200

    Repaired resign

--- src/main/java/Model/ModelOne.java
+++ src/main/java/Model/ModelOne.java
@@ -11,6 +11,7 @@ import java.util.List;
  * Created by bartek on 05.05.14.
  */
 public class ModelOne implements ModelInterface {
+    private int actualId;
     private AdapterInterface adapter;
     private boolean started;//is game started?
     private int numberOfPlayers;
@@ -20,13 +21,16 @@ public class ModelOne implements ModelInterface {
     private int currentPlayerId;
     private int numberInGame;
     private int raisingPlayerId;
-    private int enter;
+    private int ante;
     private Deck deck;
     private Deck.Card cards[];
     private int stage;
     private int resigned;
+    private int startedAmount;
+
 
     public ModelOne(AdapterInterface arg1){
+        actualId = 0;
         this.started=false;
         this.numberOfPlayers=0;
         this.players= new ArrayList<Player>();
@@ -34,6 +38,8 @@ public class ModelOne implements ModelInterface {
         this.adapter=arg1;
         this.resigned=0;
         this.cards = new Deck.Card[5];
+        this.ante=10;
+        this.startedAmount=1000;
     }//
     @Override
     public boolean isStarted() {
@@ -100,14 +106,25 @@ public class ModelOne implements ModelInterface {
     }
 
     @Override
+    public void setAnte(int arg1){
+        ante=arg1;
+    }
+
+    @Override
+    public Deck.Card[] getHandCards(int playerId){
+        return players.get(playerId).getCards();
+    }
+
+    @Override
     public void setStartedAmount(int amount) {
+        startedAmount=amount;
 
     }
 
     @Override
     public void addPlayer(String name) {
         if(!this.started) {
-            players.add(new Player(name, 1000));
+            players.add(new Player(name, startedAmount));
             numberOfPlayers = players.size();
             adapter.addPlayer(name, numberOfPlayers - 1);
         }
@@ -116,8 +133,10 @@ public class ModelOne implements ModelInterface {
     @Override
     public void removePlayer(int playerId) {
         if(players.get(playerId).getInGame()==true) numberInGame--;
-        players.remove(playerId);
-        numberOfPlayers=players.size();
+        players.get(playerId).setResigned(true);
+
+        resigned++;
+        numberOfPlayers--;
         adapter.removePlayer(playerId);
     }//
 
@@ -201,8 +220,7 @@ public class ModelOne implements ModelInterface {
         fold(playerId);
         currentPlayerId=temporaryCurrentPlayerId;
         removePlayer(playerId);
-        resigned++;
-        numberOfPlayers--;
+
     }
 
     private void won(){
@@ -220,21 +238,22 @@ public class ModelOne implements ModelInterface {
         stage=0;
         for(int i=0;i<5;i++) cards[i]=null;
         deck=new Deck();
-        enter=10;
+
         int i=0;
         for(Player p:players){
             p.setCards(deck);
-            p.setMoney(p.getMoney()- enter);
+            p.setMoney(p.getMoney()- ante);
             adapter.updatePlayerLinedCash(i, p.getMoney());
-            p.setOffer(enter);
+            p.setOffer(ante);
             adapter.updatePlayerLinedCash(i, p.getOffer());
             p.setInGame(true);
             i++;
         }
+        adapter.startNewRound();
         currentPlayerId=0;
-        this.limit= enter;
+        this.limit= ante;
         this.numberInGame=numberOfPlayers-resigned;
-        this.onTable= enter *numberInGame;
+        this.onTable= ante *numberInGame;
     }
 
 
