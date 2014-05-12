3b1822548fdd144ba86838755091cbe2cb91d67f
miron <miron.ficak@wp.pl>
2014-05-13 00:18:40 +0200

    added test to ModelOneTest2

--- src/test/main/java/Model/ModelOneTest2.java
+++ src/test/main/java/Model/ModelOneTest2.java
@@ -91,5 +91,51 @@ public class ModelOneTest2 {
         assertEquals(1600, model.getMoney(5));
         model.check(1);
 	}
+	@Test
+	public void test1() {
+		MainAdapter adapter = new MainAdapter();
+        ModelOne model= new ModelOne(adapter);
+        adapter.addModel(model);
+        //adapter.addView(new CommandLine(adapter));
+        model.setStartedAmount(11000);
+        model.setAnte(1000);
+        model.setSmallBlind(500);
+        model.setBigBlind(1000);
+        model.addPlayer("Sylwek");
+        model.addPlayer("Szymon");
+        model.addPlayer("Maciek");
+        model.addPlayer("Bartek");
+        model.addPlayer("Miron");
+        model.start();
+        assertTrue(model.isInGame(0));
+        assertTrue(model.isInGame(1));
+        assertTrue(model.isInGame(2));
+        assertTrue(model.isInGame(3));
+        assertTrue(model.isInGame(4));
+        assertEquals(9500 , model.getMoney(0));
+        assertEquals(9000 , model.getMoney(1));
+        model.check(3);
+        model.raise(4, 2000);
+        assertEquals(2 , model.getActualPlayer());
+        model.raise(2, 500);
+        assertEquals(2 , model.getActualPlayer());
+        model.raise(2, 1000);
+        assertEquals(3 , model.getActualPlayer());
+        model.check(3);
+        model.check(4);
+        model.check(0);
+        model.raise(1,1000);
+        assertEquals(8000, model.getMoney(1));
+        assertEquals(9000, model.getMoney(0));
+        assertEquals(9000, model.getMoney(2));
+        assertEquals(9000, model.getMoney(3));
+        assertEquals(9000, model.getMoney(4));
+        assertEquals(0,model.getActualStage());
+        assertEquals(2 , model.getActualPlayer());
+        model.raise(1,2000);
+        model.raise(2, 45234326);
+        model.raise(2, 3000);
+        assertEquals(6000,model.getMoney(2));
+	}
 
 }
