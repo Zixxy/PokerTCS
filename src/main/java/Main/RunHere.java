60b965410ccc69cd297f78512dd237ae8616f0df
arytmetyk <bartlomiej.drozd@uj.edu.pl>
2014-05-14 18:31:52 +0200

    Version of client

--- src/main/java/Main/RunHere.java
+++ src/main/java/Main/RunHere.java
@@ -1,6 +1,8 @@
 package main.java.Main;
 
 import main.java.Adapter.MainAdapter;
+import main.java.Communication.CommunicationModel;
+import main.java.Model.ModelInterface;
 import main.java.Model.ModelOne;
 import main.java.View.CommandLine;
 import main.java.View.*;
@@ -19,23 +21,14 @@ public class RunHere {
     public static void main(String[] args){
         config = new Config("config");
         MainAdapter adapter = new MainAdapter();
-        ViewInterface view = TableView.createTableView(args, adapter, 0);
-        ViewInterface textView = new CommandLine(adapter);
-
+        ViewInterface view = TableView.createTableView(args, adapter, config.getUserId());
         adapter.addView(view);
-        adapter.addView(textView);
-
-        ModelOne model = new ModelOne(adapter);
+        ModelInterface model = null;
+        try {
+            model = new CommunicationModel(adapter,config.getIP(),config.getPort());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         adapter.addModel(model);
-        model.setAnte(10);
-        model.setStartedAmount(2000);
-        model.setSmallBlind(10);
-        model.setBigBlind(20);
-        model.addPlayer("Sylwek");
-        model.addPlayer("Maciek");
-        model.addPlayer("Bartek");
-        model.addPlayer("Szymon");
-        model.start();
-
     }
 }
