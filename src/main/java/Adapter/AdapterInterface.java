630e355b3b53381be1644313e4cc481392db08ff
Bartlomiej Drozd <bartlomiej.drozd@uj.edu.pl>
2014-05-19 23:34:46 +0200

    Update AdapterInterface.java

--- src/main/java/Adapter/AdapterInterface.java
+++ src/main/java/Adapter/AdapterInterface.java
@@ -13,18 +13,26 @@ public interface AdapterInterface {
     public void check(int playerId);
     public void raise(int playerId, String amount);
     public void resign(int playerId);
-    public void clearTable();
-    public void sendMessage(String text);
+
+
+    public void addModel(ModelInterface mod);
+    public void addView(ViewInterface view);
+
+
+    //public Deck.Card[] getHandCards(int playerId);
+
+    public int getActualStage();
+
     public void addPlayer(String name, int id);
     public void removePlayer(int id);
     public void updatePlayerCash(int id, int cash);
     public void updatePlayerLinedCash(int id, int cash);
-    public void addModel(ModelInterface mod);
-    public void addView(ViewInterface view);
+    public void updatePlayerHand(int playerId,Deck.Card cards[]);
+    public void setPot(int pot);
     public void addThreeCards(Deck.Card cards[]);
     public void addOneCard(Deck.Card card);
+    public void clearTable();
+    public void sendMessage(String text);
     public void startNewRound();
-    public Deck.Card[] getHandCards(int playerId);
-    public void setPot(int pot);
-    public int getActualStage();
+
 }
