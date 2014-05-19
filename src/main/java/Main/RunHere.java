62c700a70100580a9675d6697638467f2ea2f48e
Bartlomiej Drozd <bartlomiej.drozd@uj.edu.pl>
2014-05-20 00:03:07 +0200

    Update RunHere.java

--- src/main/java/Main/RunHere.java
+++ src/main/java/Main/RunHere.java
@@ -2,7 +2,6 @@ package main.java.Main;
 
 import main.java.Adapter.MainAdapter;
 import main.java.Communication.CommunicationModel;
-import main.java.Communication.CommunicationView;
 import main.java.Model.ModelInterface;
 import main.java.Model.ModelOne;
 import main.java.View.CommandLine;
@@ -12,48 +11,32 @@ import main.java.View.ViewInterface;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 
 /**
 * Created by bartek on 05.05.14.
 */
-public class RunHere {
+public class RunHereClient {
     private static Config config;
-
-    public static void main(String[] args) {
-    	
+    public static void main(String[] args){
+        //config = new Config("config");
+        String ip="192.168.0.104";
+        int port=1226;
         MainAdapter adapter = new MainAdapter();
-        ViewInterface view;
-        try {
-            view = new CommunicationView(1224, adapter);
-        }
-        catch (IOException e) {
-            System.err.println("FATAL ERROR");
-            return;
-        }
-
+        ViewInterface view = TableView.createTableView(args, adapter, 0);
+        //ViewInterface view = TableView.createTableView(args, adapter, config.getUserId());
         adapter.addView(view);
-
-        ViewInterface view2 = TableView.createTableView(args, adapter, 1);
-        adapter.addView(view2);
-
-        ModelOne model = new ModelOne(adapter);
-        adapter.addModel(model);
-        //(new Scanner(System.in)).nextInt();
         ViewInterface textView = new CommandLine(adapter);
         adapter.addView(textView);
-        System.out.println("LOOOOOL");
-        model.setAnte(10);
-        model.setStartedAmount(2000);
-        model.setSmallBlind(10);
-        model.setBigBlind(20);
-        //model.addPlayer("Sylwek");
-        //model.addPlayer("Maciek");
-        //
-        model.addPlayer("Bartek");
-        model.addPlayer("Szymon");
-        model.start();
-
+        ModelInterface model = null;
+        try {
+            model = new CommunicationModel(adapter,ip,port);
+            //model = new CommunicationModel(adapter,config.getIP(),config.getPort());
+        } catch (IOException e) {
+            System.err.println("Super blad polaczenia na "+ip+":"+port);
+            //System.err.println("Super blad polaczenia na "+config.getIP()+":"+config.getPort());
+            e.printStackTrace();
+        }
+        adapter.addModel(model);
     }
 }
