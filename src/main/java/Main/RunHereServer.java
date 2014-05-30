9ad7327a15cf3e09eb50f16fcbafa3146c15a400
miron <miron.ficak@wp.pl>
2014-05-30 20:33:50 +0200

    deleted depricated main from runhereserver

--- src/main/java/Main/RunHereServer.java
+++ src/main/java/Main/RunHereServer.java
@@ -20,40 +20,4 @@ public class RunHereServer {
 		Server server = new Server(port);
 		Run.mainWindow.showTableList();
 	}
-	@Deprecated
-	public static void main(String[] args) {
-		/*
-		MainAdapter adapter = new MainAdapter();
-		ViewInterface view;
-		try {
-			view = new CommunicationView(1228, adapter);
-		}
-		catch (IOException e) {
-			System.err.println("FATAL ERROR");
-			return;
-		}
-
-		adapter.addView(view);
-
-		ViewInterface view2 = TableView.createTableView(args, adapter, 0);
-		adapter.addView(view2);
-
-		ModelOne model = new ModelOne(adapter);
-		adapter.addModel(model);
-		//(new Scanner(System.in)).nextInt();
-	ViewInterface textView = new CommandLine(adapter);
-		adapter.addView(textView);
-		System.out.println("LOOOOOL");
-		model.setAnte(10);
-		model.setStartedAmount(2000);
-		model.setSmallBlind(10);
-		model.setBigBlind(20);
-		//model.addPlayer("Sylwek");
-		//model.addPlayer("Maciek");
-		//
-		model.addPlayer("Maciek");
-		model.addPlayer("Sylwek");
-		//model.start();
-		*/
-	}
 }
